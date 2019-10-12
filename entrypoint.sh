5258ed386376c51300a4a61b50e2c823b635ce45
Yuva <yuva@codemancers.com>
2019-10-12 19:40:38 +0530

    add ability to provide values overrides file in encoded format

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,5 +17,13 @@ gcloud container clusters get-credentials $INPUT_CLUSTER_NAME \
 
 helm init --client-only
 helm repo update
-overrides=$(echo $INPUT_SET | xargs -n 1 | tr '\n' ',')
-helm upgrade --set $overrides $INPUT_NAME $INPUT_CHART_NAME
+
+if [ -n $INPUT_VALUES_FILE ]; then
+ echo $INPUT_VALUES_FILE | base64 -d > values.yaml
+else
+ touch values.yaml
+fi
+set_overrides=$(echo $INPUT_SET | xargs -n 1 | tr '\n' ',')
+
+helm upgrade --values values.yaml --set $set_overrides \
+     $INPUT_NAME $INPUT_CHART_NAME
